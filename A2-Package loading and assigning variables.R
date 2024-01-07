1a100e662bd8caba21a4e6eac581b2be223c8b0c
nidhisb05 <nidhisb@uchicago.edu>
2024-01-07 17:21:10 -0600

    renamed the file to make it shorter

---
+++ A2-Package loading and assigning variables.R
@@ -0,0 +1,22 @@
+## Loading tidyverse and papaja packages
+library(tidyverse)
+library(papaja)
+
+# Require other package
+
+# Assigning numeric variables
+numeric_variable <- 25
+print(numeric_variable)
+
+numeric_variable2 <- 5
+print(numeric_variable2)
+
+# Assigning string/character variables
+string_variable <- "gut microbiome"
+print(string_variable)
+
+string_variable2 <- "sepsis data"
+print(string_variable2)
+
+string_variable3 <- "uploading, dataset"
+print(string_variable3)
