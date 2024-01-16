88eeb8e8e52b4e5670dec7c8809207dded14eea8
nidhisb05 <nidhisb@uchicago.edu>
2024-01-15 21:29:04 -0600

    A4 create temperature_check function complete - Three conditional statements included for different temperatures temp > 0, temp == 0, and any temp < 0. @nrdowling @ysh627

--- HelloWorld Function.R
+++ HelloWorld Function.R
@@ -1,20 +1,15 @@
-#temperature_check <- function(temperature)
-
-#Defining degrees celsius, will be used in all conditional statements
-degree_celsius <- "\u00B0C"
-
 #Depending on what the temperature is different conditional statements
-temperature_check <- function(temperature) {
+temperature_check <- function(temperature)
+  {
+  degree_celsius <- "\u00B0C" #Defining degrees celsius, will be used in all conditional statements
   if (temperature > 0) #Any temperature greater than 0 
     {print(paste("It's", temperature, degree_celsius,"! Time to get the short sleeves out!"))
-  } else if (temperature == 0) #Only when the temperature is 0 
-    {
+  } else if (temperature == 0) {
     print(paste("Maybe I should start layering up! It's", temperature, degree_celsius,"!"))
   } else {
-    cat("I can't believe it's", temperature, degree_celsius, "! I am so c")
+    cat("I can't believe it's", temperature, degree_celsius,"! I am so c")
     for (i in 1:(0-temperature)) #Range of temperatures below 0, no stop, "o's" will continue to be added
       cat("o") #The number of "o's" is based on the temperature, e.g.; -2 will be "coold"
     cat("ld!!") #Using cat here combined the "cold" together without adding a line after every letter
   }
 }
-
